dd160c55af87dd567c4edcbc03bdcd015467e2aa
Andrew Northall <andrew@northall.me.uk>
2023-08-10 01:17:15 +0100

    Documentation updates

--- src/survex.rs
+++ src/survex.rs
@@ -1,4 +1,9 @@
 //! Unsafe Rust bindings to the Survex `img.c` library
+//!
+//! The functions, constants and structs contained within this module are direct bindings to
+//! `img.c` and `img.h` from the [Survex project](https://github.com/ojwb/survex). For more
+//! information about their function, refer to the source code for the aforementioned files
+//! within the Survex repository.
 
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
